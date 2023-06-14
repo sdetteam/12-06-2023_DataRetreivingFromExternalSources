6494d1ca5312a5fa9a22fe887d64b9611ca0f6f9
Umeshwaran Mohan <umeshwaran.mohan@telusinternational.com>
2023-06-14 18:38:10 +0530

    json

---
+++ JsonProject/src/test/java/com/Project/Main.java
@@ -0,0 +1,25 @@
+package com.Project;
+
+import java.io.IOException;
+
+public class Main extends TestLogin {
+
+	public static void main(String[] args) {
+	/*	String jsonFilePath = "/JsonProject/jsonfiles/employee.json";
+        String key = "username";
+
+        try {
+            String value = ReadDataFromJSONFile.getValueByKey("C:\\Users\\kalwakolu.thulasi\\Desktop\\Thulasi\\JsonProject\\jsonfiles\\employee.json");
+            if (value != null) {
+                System.out.println("Value: " + value);
+            } else {
+                System.out.println("Key not found.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
+	
+
+
